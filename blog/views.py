ef73e20d70a3d3cb8dc932778c4eb8a8a9926467
hayley <hayleykinnear93@gmail.com>
2019-05-20 15:51:16 +0100

    Add Contact Form Function. Email Working. Not Yet Visible

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from .forms import ContactForm
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
 
 # Create your views here.
 
@@ -60,6 +62,37 @@ def add_comment_to_post(request, pk):
 def contact(request):
     form_class = ContactForm
 
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the
+            # contact information
+            template = get_template('blog/contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" +'',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact')
+
     return render(request, 'blog/contact.html', {
-        'form': form_class,
-    })
+    'form': form_class,})
